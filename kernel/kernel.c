22c38132c30f18f3f24c47bc4e0a4738f54f0f6c
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-10-18 23:09:09 -0400

    Add kernel file

---
+++ kernel/kernel.c
@@ -0,0 +1,12 @@
+#include "terminal.h"
+
+void kernel_main() {
+    // Set up terminal and print a message
+    terminal_initialize();
+    terminal_writestring("Welcome to DevOS!\n");
+
+    // You can add more initialization code here later
+    while (1) {
+        // Infinite loop to keep the OS running
+    }
+}
